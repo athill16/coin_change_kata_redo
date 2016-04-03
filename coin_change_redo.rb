b53677f94ceaa8f831d15fced4e1641ecbc0115c
Aaron Hill <aaronthill16@gmail.com>
2016-04-03 14:00:44 -0400

    Added test for 6

--- coin_change_redo.rb
+++ coin_change_redo.rb
@@ -1,7 +1,9 @@
 def evaluate_change(number)
 	if number < 5
 		{"Quarters" => 0, "Dimes" => 0, "Nickels" => 0, "Pennies" => number}
-	else
+	elsif number == 5
 		{"Quarters" => 0, "Dimes" => 0, "Nickels" => 1, "Pennies" => 0}
+	else
+		{"Quarters" => 0, "Dimes" => 0, "Nickels" => 1, "Pennies" => 1}
 	end
 end
--- coin_change_redo_tests.rb
+++ coin_change_redo_tests.rb
@@ -3,12 +3,16 @@ require_relative "coin_change_redo.rb"
 
 class TestCoinChange < Minitest::Test
 
-	def test_change_is_one_penny
+	def test_change_for_1
 		assert_equal({"Quarters" => 0, "Dimes" => 0, "Nickels" => 0, "Pennies" => 1}, evaluate_change(1))
 	end
 
-	def test_change_is_one_nickel
+	def test_change_for_5
 		assert_equal({"Quarters" => 0, "Dimes" => 0, "Nickels" => 1, "Pennies" => 0}, evaluate_change(5))
+	end
+
+	def test_change_for_6
+		assert_equal({"Quarters" => 0, "Dimes" => 0, "Nickels" => 1, "Pennies" => 1}, evaluate_change(6))
 	end
 
 end
