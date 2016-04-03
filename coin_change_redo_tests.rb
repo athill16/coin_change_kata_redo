d63b78422bc06a2a4a4f9f9cfca232444576f5e6
Aaron Hill <aaronthill16@gmail.com>
2016-04-03 14:18:20 -0400

    Added random tests to confirm it works correctly

--- coin_change_redo_tests.rb
+++ coin_change_redo_tests.rb
@@ -23,4 +23,30 @@ class TestCoinChange < Minitest::Test
 		assert_equal({"Quarters" => 0, "Dimes" => 1, "Nickels" => 0, "Pennies" => 1}, evaluate_change(11))
 	end
 
+	def test_change_for_25
+		assert_equal({"Quarters" => 1, "Dimes" => 0, "Nickels" => 0, "Pennies" => 0}, evaluate_change(25))
+	end
+
+	def test_change_for_26
+		assert_equal({"Quarters" => 1, "Dimes" => 0, "Nickels" => 0, "Pennies" => 1}, evaluate_change(26))
+	end
+
+	def test_change_for_75
+		assert_equal({"Quarters" => 3, "Dimes" => 0, "Nickels" => 0, "Pennies" => 0}, evaluate_change(75))
+	end
+
+	def test_change_for_91
+		assert_equal({"Quarters" => 3, "Dimes" => 1, "Nickels" => 1, "Pennies" => 1}, evaluate_change(91))
+	end
+
+
 end
+
+
+
+
+
+
+
+
+
