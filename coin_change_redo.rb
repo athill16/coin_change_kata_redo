2d54fab10b07a9710f5e9465b197b2652a0c0c23
Aaron Hill <aaronthill16@gmail.com>
2016-04-03 13:57:09 -0400

    Added test for one penny

--- coin_change_redo.rb
+++ coin_change_redo.rb
@@ -1,5 +1,5 @@
 def evaluate_change(number)
-
-
+	
+	{"Quarters" => 0, "Dimes" => 0, "Nickels" => 0, "Pennies" => number}
 
 end
--- coin_change_redo_tests.rb
+++ coin_change_redo_tests.rb
@@ -3,4 +3,8 @@ require_relative "coin_change_redo.rb"
 
 class TestCoinChange < Minitest::Test
 
-	def test_
+	def test_change_is_one_penny
+		assert_equal({"Quarters" => 0, "Dimes" => 0, "Nickels" => 0, "Pennies" => 1}, evaluate_change(1))
+	end
+
+end
